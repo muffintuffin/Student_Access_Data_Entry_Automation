e8f6093b52ff0a2d861140d90988eb8ac57af285
David <45515409+muffintuffin@users.noreply.github.com>
2023-08-31 16:39:24 -0400

    added feature that matches first name of both the excel and pdf, now to work on implementing it for last name in case there's multiple people with the same first name.

--- data_processing.py
+++ data_processing.py
@@ -6,11 +6,29 @@
 import pandas as pd
 from pdfminer.high_level import extract_text
 import glob
-import re
 
 
 def read_excel_data(excel_file):
     return pd.read_excel(excel_file)
+
+
+# function that will create a dictonary from the excel data
+def extract_excel_data(excel_file):
+    df = read_excel_data(excel_file)
+    excel_dict = {}
+    for index, row in df.iterrows():
+        information = (str(row['First Name'])).lower().strip()
+        excel_dict[information] = {
+            'first name': str(row['First Name']),
+            'middle name': str(row['Middle Name']),
+            'last name': str(row['Last Name']),
+            'bnumber': str(row['B Number']),
+        }
+    return excel_dict
+#print(extract_excel_data('Sample Bnumber List.xlsx'))
+
+
+#(extract_excel_data('Sample Bnumber List.xlsx'))  # test print to see if all items are being printed.
 
 
 def extract_pdf_data(pdf_files):
@@ -25,7 +43,9 @@ def extract_pdf_data(pdf_files):
         name_indices = [i for i, line in enumerate(lines) if 'Name' in line]
         name_index = name_indices[0] if name_indices else None
         if name_index is not None and name_index + 2 < len(lines):
-            participant['name'] = lines[name_index + 2].strip()
+            full_name = lines[name_index + 2].strip()
+            first_name = full_name.split(' ')[0]  # only gets the first word of the name
+            participant['name'] = first_name.lower().strip()
 
         gender_indices = [i for i, line in enumerate(lines) if 'Gender' in line]
         gender_index = gender_indices[0] if gender_indices else None
@@ -46,9 +66,29 @@ def extract_pdf_data(pdf_files):
         # print(text) #TODO: Delete after use
 
     return pdf_data
+#print(extract_pdf_data(glob.glob("PDF Files/*.pdf")))
 
 
-def process_data(df, pdf_files):
+
+
+
+
+
+# function that will cross-reference the list of dictionaries from the PDFs with the Excel dictionary.
+def match_data(pdf_data, excel_dict):
+    combined_data = []
+    for pdf_participant in pdf_data:
+        # tries to find a match in the Excel dictionary
+        excel_participant = excel_dict.get(pdf_participant.get('name', '').lower())
+        if excel_participant:
+            #combines dictionaries
+            combined_info = {**excel_participant, **pdf_participant}
+            combined_data.append(combined_info)
+    print(combined_data)
+    return combined_data
+
+
+'''def process_data(df, pdf_files):
     pdf_data = extract_pdf_data(pdf_files)
     excel_data = []
     student_info_list = []
@@ -76,56 +116,10 @@ def process_data(df, pdf_files):
         student_info_list.append(student_info)
 
     return student_info_list
+'''  # function that will create a list of dictionaries from the excel and pdf data
 
-
-if __name__ == '__main__':  # the following function is a test function that only runs within the file itself.
-    excel_file = 'Sample Bnumber List.xlsx'
-    pdf_files = glob.glob("PDF Files/*.pdf")
-    df = read_excel_data(excel_file)
-
-    # Read all the PDF files
-    pdf_data = extract_pdf_data(pdf_files)
-
-    # Create a dictionary from Excel to easily lookup
-    excel_dict = {}
-    for index, row in df.iterrows():
-        name = str(row['First Name']) + ' ' + str(row['Middle Name']) + ' ' + str(row['Last Name'])
-        excel_dict[name] = {
-            'first name': str(row['First Name']),
-            'middle name': str(row['Middle Name']),
-            'last name': str(row['Last Name']),
-            'bnumber': str(row['B Number']),
-        }
-
-    # Match PDF and Excel based on name
-    student_info_list = []
-    for pdf_participant in pdf_data:
-        if 'name' in pdf_participant:  # Confirm that the 'name' key exists
-            name_from_pdf = pdf_participant['name'].strip()
-            name_from_pdf = name_from_pdf.strip().lower()
-
-            excel_participant = excel_dict.get(name_from_pdf, {})
-
-            print(f"Name from PDF: {name_from_pdf}")
-            print(f"Excel Participant: {excel_participant}")
-            print(f"Excel Dict Sample: {list(excel_dict.keys())[:5]}")
-
-            if not excel_participant:  # If name didn't match, let's look for the next line
-                for i, (key, value) in enumerate(excel_dict.items()):
-                    if name_from_pdf in value['first name'] + ' ' + value['last name']:
-                        excel_participant = value
-                        break
-
-        if excel_participant:
-            student_info = {
-                'first name': excel_participant['first name'],
-                'middle name': excel_participant['middle name'],
-                'last name': excel_participant['last name'],
-                'bnumber': excel_participant['bnumber'],
-                'gender': pdf_participant['gender'],
-                'Date of Birth': pdf_participant['birthday'],
-                'Race': pdf_participant['race'],
-            }
-            student_info_list.append(student_info)
-
-    print(student_info_list)
+if __name__ == "__main__":
+    excel_data = extract_excel_data('Sample Bnumber List.xlsx')
+    pdf_data = extract_pdf_data(glob.glob("PDF Files/*.pdf"))
+    combined_data = match_data(pdf_data, excel_data)
+    print("Combined Data: ", combined_data)
